1f20f05762f06a6ebc7be8d7777d70713ebcfbd6
sapardo10 <sa.pardo10@uniandes.edu.co>
2018-03-06 03:45:26 -0500

    para el deploy

--- server.js
+++ server.js
@@ -100,11 +100,6 @@ app.use(function(req,res,next){
 ---------------------------API------------------------------
 -------------------------------------------------------------------------
 */
-app.get('/', (req, res) => {
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-  }
-});
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
