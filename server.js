f0e7e1fc2f76652b4e9b812f63b49b0c2bacd51e
sapardo10 <sa.pardo10@uniandes.edu.co>
2018-03-06 03:42:02 -0500

    para el deploy

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const port = process.env.PORT || 3001;
 const formidable = require('formidable');
 const Binary = require('mongodb').Binary;
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+};
+
 /*
 -------------------------------------------------------------------------
 -----------------------DATABASE CONFIGURATION----------------------------
@@ -96,6 +100,11 @@ app.use(function(req,res,next){
 ---------------------------API------------------------------
 -------------------------------------------------------------------------
 */
+app.get('/', (req, res) => {
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
+});
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
